fdc3963f34bcebed7a826a6a5711d39fa25b0153
Gabriel Bider <1554615+silkfire@users.noreply.github.com>
2023-05-26 23:13:19 +0200

    Bump packages

--- LineEndingsUnifier/Properties/AssemblyInfo.cs
+++ LineEndingsUnifier/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Gabriel Bider, Jakub Bielawa")]
 [assembly: AssemblyProduct("Line Endings Unifier")]
-[assembly: AssemblyCopyright("© 2022")]
+[assembly: AssemblyCopyright("© 2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]   
 [assembly: ComVisible(false)]     
@@ -28,8 +28,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.0.1.0")]
+[assembly: AssemblyFileVersion("2.0.1.0")]
 
 
 
