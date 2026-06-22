8471d066c6f2db685f34556f640b9bef913d6fdc
Gabriel Bider <1554615+silkfire@users.noreply.github.com>
2026-06-22 22:53:06 +0200

    Remove now-unused per-platform line ending finder factories
    The Windows/Linux/Macintosh finder factories were only used by the Dominant
    counting code, which now classifies the already-collected matches instead.
    Removing their fields, construction, and accessors avoids creating three
    finder factories that are never used.

--- LineEndingsUnifier/LineEndingFinderFactoryProvider.cs
+++ LineEndingsUnifier/LineEndingFinderFactoryProvider.cs
@@ -7,10 +7,6 @@
         private readonly IFinderFactory _lineEndingFinderFactory;
         private readonly IFinderFactory _consecutiveWhiteSpaceFollowedByLineEndingFinderFactory;
 
-        private readonly IFinderFactory _windowsLineEndingFinderFactory;
-        private readonly IFinderFactory _linuxLineEndingFinderFactory;
-        private readonly IFinderFactory _macintoshLineEndingFinderFactory;
-
         private readonly IFinderFactory _nonWindowsLineEndingFinderFactory;
         private readonly IFinderFactory _nonLinuxLineEndingFinderFactory;
         private readonly IFinderFactory _nonMacintoshLineEndingFinderFactory;
@@ -20,10 +16,6 @@
             _lineEndingFinderFactory                                = findService.CreateFinderFactory(LineEndingSearchPattern.Any,                                FindOptions.UseRegularExpressions);
             _consecutiveWhiteSpaceFollowedByLineEndingFinderFactory = findService.CreateFinderFactory(LineEndingSearchPattern.ConsecutiveWhiteSpaceFollowedByAny, FindOptions.UseRegularExpressions);
 
-            _windowsLineEndingFinderFactory                         = findService.CreateFinderFactory(LineEndingSearchPattern.Windows,                            FindOptions.UseRegularExpressions);
-            _linuxLineEndingFinderFactory                           = findService.CreateFinderFactory(LineEndingSearchPattern.Linux,                              FindOptions.UseRegularExpressions);
-            _macintoshLineEndingFinderFactory                       = findService.CreateFinderFactory(LineEndingSearchPattern.Macintosh,                          FindOptions.UseRegularExpressions);
-
             _nonWindowsLineEndingFinderFactory                      = findService.CreateFinderFactory(LineEndingSearchPattern.NonWindows,                         FindOptions.UseRegularExpressions);
             _nonLinuxLineEndingFinderFactory                        = findService.CreateFinderFactory(LineEndingSearchPattern.NonLinux,                           FindOptions.UseRegularExpressions);
             _nonMacintoshLineEndingFinderFactory                    = findService.CreateFinderFactory(LineEndingSearchPattern.NonMacintosh,                       FindOptions.UseRegularExpressions);
@@ -31,10 +23,6 @@
 
         public IFinderFactory GetLineEndingFinderFactory() => _lineEndingFinderFactory;
         public IFinderFactory GetConsecutiveWhiteSpaceFollowedByLineEndingFinderFactory() => _consecutiveWhiteSpaceFollowedByLineEndingFinderFactory;
-
-        public IFinderFactory GetWindowsLineEndingFinderFactory() => _windowsLineEndingFinderFactory;
-        public IFinderFactory GetLinuxLineEndingFinderFactory() => _linuxLineEndingFinderFactory;
-        public IFinderFactory GetMacintoshLineEndingFinderFactory() => _macintoshLineEndingFinderFactory;
 
         public IFinderFactory GetNonWindowsLineEndingFinderFactory() => _nonWindowsLineEndingFinderFactory;
         public IFinderFactory GetNonLinuxLineEndingFinderFactory() => _nonLinuxLineEndingFinderFactory;
